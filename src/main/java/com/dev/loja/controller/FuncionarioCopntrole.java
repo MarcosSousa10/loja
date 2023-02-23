567bdeff8443f7805a3a36756872c92d97af74c4
Sr-Marcos <marcospegodesousa10@gmail.com>
2023-02-23 10:23:09 -0300

    layout

--- src/main/java/com/dev/loja/controller/FuncionarioCopntrole.java
+++ src/main/java/com/dev/loja/controller/FuncionarioCopntrole.java
@@ -1,13 +1,15 @@
 package com.dev.loja.controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
-
 import com.dev.loja.modelos.Funcionario;
 import com.dev.loja.reposiotiros.Funcionariorepositorio;
 @Controller
@@ -19,6 +21,17 @@ public class FuncionarioCopntrole {
         ModelAndView mv = new ModelAndView("administrativo/funcionarios/cadastro");
         mv.addObject("funcionario", funcionario);
         return mv;
+    }
+    @GetMapping("/administrativo/funcionarios/editar/{id}")
+    public ModelAndView editar (@PathVariable("id") Long id){
+        Optional<Funcionario> funcionario= funcionariorepositorio.findById(id);
+        return cadastrar(funcionario.get());
+    }
+    @GetMapping("/administrativo/funcionarios/remover/{id}")
+    public ModelAndView remover (@PathVariable("id") Long id){
+        Optional<Funcionario> funcionario= funcionariorepositorio.findById(id);
+        funcionariorepositorio.delete(funcionario.get());
+        return listar();
     }
     @GetMapping("/administrativo/funcionarios/listar")
     public ModelAndView listar(){
